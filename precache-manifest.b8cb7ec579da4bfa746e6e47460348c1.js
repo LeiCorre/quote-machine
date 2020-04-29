a4d3b719e070ff5973b2592fa1ecf85b7bcdcf29
LeiCorre <jen.y.loaa@gmail.com>
2020-04-29 16:53:09 -0600

    Updates

---
+++ precache-manifest.b8cb7ec579da4bfa746e6e47460348c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8ffe3b3ae169c78354533514576508",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "86b6fb8f90616562f813",
+    "url": "/quote-machine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f8997a77210b7a08203",
+    "url": "/quote-machine/static/js/2.ba4fe2a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.ba4fe2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b6fb8f90616562f813",
+    "url": "/quote-machine/static/js/main.26bbc850.chunk.js"
+  },
+  {
+    "revision": "4de20eb4d0cefe3f0eda",
+    "url": "/quote-machine/static/js/runtime-main.d37b2625.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
